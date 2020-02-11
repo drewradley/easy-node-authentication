20b50d585f1215cee195d8ed2faf45d833f00aab
Drew <drewradley@gmail.com>
2020-02-10 16:03:22 -0800

    OOMPHDB ver1

--- app/routes.js
+++ app/routes.js
@@ -83,7 +83,7 @@ module.exports = function(app, passport) {
 
         // process the signup form
         app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect : '/signup', // redirect to the secure profile section
+            successRedirect : '/login', // redirect to the secure profile section
             failureRedirect : '/signup', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
         }));
